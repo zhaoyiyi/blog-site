38d01e91877d251b45117532b628918e81878bcc
Yi Zhao <yi@yizhao.me>
2023-03-25 20:08:51 -0400

    vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
   css: ['~/assets/css/main.css'],
   app: {
     head: {
